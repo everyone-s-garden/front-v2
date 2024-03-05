49dd635a790f95bc02cbec27fde3a9c89d5a3655
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-03-05 20:36:40 +0900

    chore: 스토리북 세팅

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,17 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+import { chakraTheme } from '../src/styles/theme';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
+    },
+    chakra: {
+      theme: chakraTheme,
     },
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -19,5 +19,8 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
+    watch: {
+      usePolling: true,
+    },
   },
 });
