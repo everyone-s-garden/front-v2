fe02a99f5d67f19c0ea643307fa462e287b51f27
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-03-06 18:07:54 +0900

    chore: husky, commitlint 세팅

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-undef
 module.exports = {
+  extends: ['@commitlint/config-conventional'],
   plugins: ['commitlint-plugin-function-rules'],
   rules: {
     'header-max-length': [2, 'always', 100],
