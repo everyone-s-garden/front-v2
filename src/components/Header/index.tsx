94e3f5b9f142375e723780b9b850fb65a1f6b320
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-03-20 18:51:18 +0900

    refactor: 헤더에 바뀐 탭 컴포넌트 적용

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,13 +4,13 @@ import {
   Link as ChakraLink,
   Button,
   Container,
-  Show,
   Box,
 } from '@chakra-ui/react';
+import { nanoid } from 'nanoid';
 import { Link as ReactRouterLink, useNavigate } from 'react-router-dom';
 import { ProfileIcon } from '@/assets/icons';
 import { LogoHorizon } from '@/assets/images';
-import NavTab from './NavTab';
+import Tab from '../Tab/Tab';
 import PostMenu from './PostMenu';
 import { headerNavLinks } from './constants';
 
@@ -19,50 +19,47 @@ const Header = () => {
 
   return (
     <>
-      <Show above="tablet">
-        <Container
-          as="header"
-          h="108px"
-          maxW="1167px"
-          mx="auto"
-          display="flex"
-          justifyContent="space-between"
-          alignItems="center"
-          px="20px"
-        >
-          <Flex gap="36px" align="center">
-            <Image
-              src={LogoHorizon}
-              alt="로고"
-              maxW="163px"
-              h="auto"
-              cursor="pointer"
-              onClick={() => navigate('/')}
-            />
-            {headerNavLinks.slice(1).map(({ href, name }) => (
-              <ChakraLink
-                as={ReactRouterLink}
-                to={href}
-                key={href}
-                fontWeight="semiBold"
-                fontSize="18px"
-                _hover={{ textDecoration: 'none' }}
-              >
-                {name}
-              </ChakraLink>
-            ))}
-          </Flex>
-          <Flex gap="30px">
-            <Button fontWeight="regular" bg="none" _hover={{ bg: 'none' }}>
-              로그인 / 회원가입
-            </Button>
-            <PostMenu />
-          </Flex>
-        </Container>
-      </Show>
-      <Show below="tablet">
-        <MobileHeader />
-      </Show>
+      <Container
+        as="header"
+        h="108px"
+        maxW="1167px"
+        mx="auto"
+        display="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        px="20px"
+        hideBelow="tablet"
+      >
+        <Flex gap="36px" align="center">
+          <Image
+            src={LogoHorizon}
+            alt="로고"
+            maxW="163px"
+            h="auto"
+            cursor="pointer"
+            onClick={() => navigate('/')}
+          />
+          {headerNavLinks.slice(1).map(({ href, tabName }) => (
+            <ChakraLink
+              as={ReactRouterLink}
+              to={href}
+              key={nanoid()}
+              fontWeight="semiBold"
+              fontSize="18px"
+              _hover={{ textDecoration: 'none' }}
+            >
+              {tabName}
+            </ChakraLink>
+          ))}
+        </Flex>
+        <Flex gap="30px">
+          <Button fontWeight="regular" bg="none" _hover={{ bg: 'none' }}>
+            로그인 / 회원가입
+          </Button>
+          <PostMenu />
+        </Flex>
+      </Container>
+      <MobileHeader />
     </>
   );
 };
@@ -77,6 +74,7 @@ const MobileHeader = () => {
       display="flex"
       flexDir="column"
       justifyContent="space-between"
+      hideFrom="tablet"
     >
       <Flex justify="space-between" pt="15px" px="20px">
         <Image
@@ -88,7 +86,7 @@ const MobileHeader = () => {
         />
         <ProfileIcon />
       </Flex>
-      <NavTab />
+      <Tab tabsData={headerNavLinks} color="orange" tabWidth="full" />
     </Box>
   );
 };
