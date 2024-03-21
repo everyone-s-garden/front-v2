6e7c8f51bec123d6cff3e3f0157b6fcc9251c383
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-03-21 09:43:12 +0900

    feat: 모바일 메뉴 컴포넌트 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from './Header/Header';
 export { default as ImageSelector } from './ImageSelector/ImageSelector';
+export { default as BottomMenu } from './BottomMenu/BottomMenu';
 
 export * from './Dropdown/Dropdown';
