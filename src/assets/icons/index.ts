5242ded0e919cde08f23c0b034202b7e493734a9
Devinix <121074744+Devinix00@users.noreply.github.com>
2024-03-22 11:08:21 +0900

    모달 컴포넌트 구현 (#26)
    * feat: 공통 모달 컴포넌트 구현
    
    * feat: storybook 작성
    
    * style: 모달 컴포넌트 디자인 수정
    
    * refactor: Props 옵셔널로 적용
    
    * chore: Modal 컴포넌트 export
    
    * refactor: buttonContent 타입 수정
    
    * refactor: isOpen, onClose를 props로 받게 수정
    
    * refactor: ModalContent w 속성 제거

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,3 +4,5 @@ export { default as PrevIcon } from './arrow-left.svg?react';
 export { default as NextIcon } from './arrow-right.svg?react';
 export { default as CameraIcon } from './camera.svg?react';
 export { default as CloseIcon } from './close.svg?react';
+export { default as ModalPlusIcon } from './modal-plus-button.svg?react';
+export { default as LocationIcon } from './location-icon.svg?react';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Header } from './Header/Header';
 export { default as ImageSelector } from './ImageSelector/ImageSelector';
+export { default as Modal } from './Modal/Modal';
 
 export * from './Dropdown/Dropdown';
