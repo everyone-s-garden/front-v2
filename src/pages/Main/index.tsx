e126db25a3611a6c200fd89e4735a505acc239db
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-03-25 10:44:15 +0900

    refactor: 필요없는 api 호출 제거

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,9 +1,6 @@
 import { Box } from '@chakra-ui/react';
-import { useGetUserInfo } from '@/services/user/query';
 
 const Main = () => {
-  const { data } = useGetUserInfo(1);
-
   return (
     <div>
       <Box height="300vh"></Box>
