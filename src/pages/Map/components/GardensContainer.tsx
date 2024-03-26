f36f84dda7d84354bff6d8521e208305f7f0af66
Devinix00 <dpldpl123456@naver.com>
2024-03-26 11:25:11 +0900

    feat: GardensContainer 텃밭 없는 상태 퍼블리싱

--- src/pages/Map/components/GardensContainer.tsx
+++ src/pages/Map/components/GardensContainer.tsx
@@ -1,5 +1,6 @@
-import { chakra } from '@chakra-ui/react';
+import { Box, Flex, Image, Text, chakra } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
+import { MapNoGardenImg } from '@/assets/images';
 
 const GardenContainer = chakra(motion.div);
 
@@ -23,7 +24,26 @@ const GardensContainer = () => {
         x: 379,
         transition: { type: 'tween' },
       }}
-    ></GardenContainer>
+    >
+      <Flex
+        flexDir="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="16px"
+        h="100%"
+      >
+        <Image src={MapNoGardenImg} w="66px" h="66px" />
+
+        <Box>
+          <Text fontWeight="semibold" fontSize="18px" color="#80AC49">
+            해당 지역에는 텃밭이 없어요!
+          </Text>
+          <Text fontWeight="regular" fontSize="16px" color="#80AC49">
+            지도를 다른 곳으로 움직여보세요.
+          </Text>
+        </Box>
+      </Flex>
+    </GardenContainer>
   );
 };
 
