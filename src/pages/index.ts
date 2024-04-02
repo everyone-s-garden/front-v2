4e607a40e337849c8cdf945e56f3ace36999a42c
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-04-02 13:16:17 +0900

    style: 속닥속닥 작성 페이지 마크업

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,3 +3,4 @@ export { default as GardenEdit } from './Garden/GardenEdit/GardenEdit';
 export { default as MyGardenEdit } from './Garden/MyGardenEdit/MyGardenEdit';
 export { default as CommunityMain } from './Community/CommunityMain/CommunityMain';
 export { default as CommunityDetail } from './Community/CommunityDetail/CommunityDetail';
+export { default as CommunityEdit } from './Community/CommunityEdit/CommunityEdit';
