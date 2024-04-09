b625437967c0669592bf4bc17efc92371b2bdab3
Devinix00 <dpldpl123456@naver.com>
2024-04-09 12:31:43 +0900

    style: map경로에서 헤더 상단 고정 해제

--- src/routes/Layout.tsx
+++ src/routes/Layout.tsx
@@ -1,11 +1,19 @@
 import { Box, Container } from '@chakra-ui/react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Header } from '@/components';
+import { PATH } from './constants';
 
 const Layout = () => {
+  const location = useLocation();
+
   return (
     <Container minH={'100svh'} maxW={'auto'} px={0}>
-      <Box pos="sticky" top={0} bg="white" zIndex={10}>
+      <Box
+        pos={location.pathname === PATH.MAP.MAIN ? 'static' : 'sticky'}
+        top={0}
+        bg="white"
+        zIndex={10}
+      >
         <Header />
       </Box>
       <Outlet />
