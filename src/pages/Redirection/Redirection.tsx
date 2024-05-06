97e9c3d30c8edb784ae1318528aee2f24e31bc4d
Devinix00 <dpldpl123456@naver.com>
2024-05-06 18:17:00 +0900

    feat: 카카오 로그인 api 통신

--- src/pages/Redirection/Redirection.tsx
+++ src/pages/Redirection/Redirection.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import loginApi from '@/services/login/api';
 
 function Redirection() {
   const navigate = useNavigate();
@@ -7,7 +8,12 @@ function Redirection() {
   const code = serachParams.get('code');
   const deniedCode = serachParams.get('error');
 
-  console.log(code);
+  const kakaoRedirectUri = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+
+  useEffect(() => {
+    const data = loginApi.kakaoLogin(code, kakaoRedirectUri);
+    console.log(data);
+  }, []);
 
   useEffect(() => {
     if (deniedCode === 'access_denied') {
---
+++ src/services/login/api.ts
@@ -0,0 +1,14 @@
+import apiClient from '@/api/apiClient';
+
+const loginApi = {
+  kakaoLogin: async (code: string | null, redirectUri: string) => {
+    const response = await apiClient.post('/v1/auth/kakao', {
+      code,
+      redirectUri,
+    });
+
+    return response.data;
+  },
+};
+
+export default loginApi;
