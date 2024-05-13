74689e5886b8dc855b102d994b1b0d679b5e1e44
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-05-13 21:03:21 +0900

    refactor: env 수정 및 로그인 로직 수정

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,13 +6,14 @@ import LoginLink from './components/LoginLink';
 
 const Login = () => {
   const navigate = useNavigate();
+  const origin = window.location.origin;
 
   const KAKAO_REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
-  const KAKAO_REDIRECT_URI = import.meta.env.VITE_KAKAO_REDIRECT_URI;
+  const KAKAO_REDIRECT_URI = `${origin}/login/oauth2/kakao`;
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}`;
 
   const NAVER_CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID;
-  const NAVER_REDIRECT_URI = import.meta.env.VITE_NAVER_REDIRECT_URI;
+  const NAVER_REDIRECT_URI = `${origin}/login/oauth2/naver`;
   const NAVER_AUTH_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${NAVER_CLIENT_ID}&redirect_uri=${NAVER_REDIRECT_URI}`;
 
   return (
