6d2af675f4789fb683ba1c2156793fe6e5441b6e
HongGunWoo <foxrain.gg@gmail.com>
2024-06-26 01:03:14 +0900

    style: Avatar 컴포넌트 기본 아이콘 제거

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -5,7 +5,7 @@ const AvatarComponent = ({ src, loading = 'lazy', ...rest }: AvatarProps) => {
   //유저 이미지 없을 경우 기본 농부 이미지
   const profileSrc = src || DefaultProfile;
 
-  return <Avatar src={profileSrc} loading={loading} {...rest} />;
+  return <Avatar src={profileSrc} loading={loading} {...rest} icon={<></>} />;
 };
 
 export default AvatarComponent;
