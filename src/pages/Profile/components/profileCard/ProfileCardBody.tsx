c85eb02b2dc9a3ba34a084bb69ad732eafdd213e
Devinix <121074744+Devinix00@users.noreply.github.com>
2024-07-01 12:53:44 +0900

    style: ProfileCard 태블릿 사이즈 퍼블리싱

--- src/pages/Profile/components/profileCard/ProfileCardBody.tsx
+++ src/pages/Profile/components/profileCard/ProfileCardBody.tsx
@@ -4,30 +4,30 @@ import { DefaultProfile } from '@/assets/images';
 const ProfileCardBody = () => {
   return (
     <Flex
-      gap={{ mobile: '12px', tablet: '10px' }}
+      gap={{ mobile: '12px', tablet: '7.82px', desktop: '10px' }}
       flexDir={{ mobile: 'row', tablet: 'column' }}
       justifyContent={{ mobile: 'center', tablet: '' }}
       alignItems={{ mobile: 'center', tablet: '' }}
-      mt={{ mobile: '28px', tablet: '49px' }}
+      mt={{ mobile: '28px', tablet: '38.17px', desktop: '49px' }}
       mb={{ mobile: '20px', tablet: '24px' }}
     >
       <Image
         src={DefaultProfile}
         alt="기본 이미지"
-        w={{ mobile: '70px', tablet: '94px' }}
-        h={{ mobile: '70px', tablet: '94px' }}
+        w={{ mobile: '70px', tablet: '73.52px', desktop: '94px' }}
+        h={{ mobile: '70px', tablet: '73.52px', desktop: '94px' }}
         borderRadius="50%"
       />
       <Flex
         flexDir="column"
         justifyContent={{ mobile: 'cetner', tablet: '' }}
         alignItems={{ mobile: '', tablet: 'center' }}
-        gap={{ mobile: '8px', tablet: '10px' }}
+        gap={{ mobile: '8px', tablet: '7.82px', desktop: '10px' }}
       >
         <Box
-          fontSize="14px"
+          fontSize={{ mobile: '14px', tablet: '10.95px', desktop: '14px' }}
           fontWeight="semiBold"
-          py="4px"
+          py={{ motile: '4px', tablet: '3.13px', desktop: '4px' }}
           px={{ mobile: '12px', tablet: '10px' }}
           borderRadius="10px"
           bgColor="orange.500"
@@ -37,7 +37,11 @@ const ProfileCardBody = () => {
         >
           텃린이
         </Box>
-        <Text color="orange.500" fontSize="10px" fontWeight="medium">
+        <Text
+          color="orange.500"
+          fontSize={{ mobile: '10px', tablet: '7.82px', desktop: '10px' }}
+          fontWeight="medium"
+        >
           rrgy980@naver.com
         </Text>
       </Flex>
--- src/pages/Profile/components/profileCard/ProfileCardFooter.tsx
+++ src/pages/Profile/components/profileCard/ProfileCardFooter.tsx
@@ -5,17 +5,25 @@ const ProfileCardFooter = () => {
   return (
     <Flex
       w="full"
-      gap="12px"
+      gap={{ mobile: '12px', tablet: '9px', desktop: '12px' }}
       justifyContent="center"
       alignItems="center"
       mx="auto"
       bgColor="orange.500"
-      h={{ mobile: '48px', tablet: '56px' }}
+      h={{ mobile: '48px', tablet: '43.8px', desktop: '56px' }}
       pos="absolute"
       bottom="0"
     >
-      <Icon as={SeedIcon} w="38px" h="36px" />
-      <Text fontSize="16px" color="white" fontWeight="semiBold">
+      <Icon
+        as={SeedIcon}
+        w={{ mobile: '38px', tablet: '29.718px', desktop: '38px' }}
+        h={{ mobile: '36px', tablet: '28.159px', desktop: '36px' }}
+      />
+      <Text
+        fontSize={{ mobile: '16px', tablet: '12.51px', desktop: '16px' }}
+        color="white"
+        fontWeight="semiBold"
+      >
         씨앗 등급
       </Text>
     </Flex>
