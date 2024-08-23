4e0d15fb69e60695af89688861d4264aecc701a7
HongGunWoo <foxrain.gg@gmail.com>
2024-08-24 03:27:58 +0900

    feat: weather type 정리

--- src/services/weather/types.ts
+++ src/services/weather/types.ts
@@ -7,19 +7,38 @@ export type WeatherValue =
   | '소나기'
   | '흐림';
 
+export type RegionName =
+  | '강원도'
+  | '서울특별시'
+  | '경기도'
+  | '경상남도'
+  | '경상북도'
+  | '광주광역시'
+  | '대구광역시'
+  | '대전광역시'
+  | '부산광역시'
+  | '세종특별자치시'
+  | '울산광역시'
+  | '인천광역시'
+  | '전라남도'
+  | '전라북도'
+  | '제주특별자치도'
+  | '충청남도'
+  | '충청북도';
+
 export interface RegionWeather {
-  regionName: string;
+  regionName: RegionName;
   skyValue: WeatherValue;
   temperatureValue: string;
 }
 
 export interface WeeklyWeatherData {
-  regionName: string;
+  regionName: RegionName;
   status: WeatherValue[];
 }
 
 export interface HourlyWeatherData {
-  regionName: string;
+  regionName: RegionName;
   weatherTimeResponses: {
     baseDate: string;
     temperature: string;
