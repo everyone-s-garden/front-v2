10e777ff3c61b838ed4b98860565f837ad1c9cd2
HongGunWoo <foxrain.gg@gmail.com>
2024-10-15 20:30:43 +0900

    refactor: 화면 크기에 따른 이미지 렌더링 방식 변경

--- src/pages/Main/components/Banner.tsx
+++ src/pages/Main/components/Banner.tsx
@@ -1,7 +1,6 @@
-import { Flex, Hide, Image, Show } from '@chakra-ui/react';
-import { Fragment } from 'react';
-import { ImageSlider } from '@/components';
+import { Flex, Image, useMediaQuery } from '@chakra-ui/react';
 import { mainBanner, mainBannerMobile } from '@/assets/images/banners';
+import { devices } from '@/styles/theme';
 
 const BANNER_INFO = [
   {
@@ -12,31 +11,16 @@ const BANNER_INFO = [
 ];
 
 const Banner = () => {
+  const [isLargerThanTablet] = useMediaQuery(devices.tablet);
+
   return (
-    <ImageSlider arrowStyle="plain" numberOfSlides={BANNER_INFO.length}>
-      {BANNER_INFO.map(({ pc, bgColor }, idx) => (
-        <Fragment key={idx}>
-          <Flex justifyContent="center" bg={bgColor}>
-            <Show above="tablet">
-              <Image
-                hideFrom="mobile"
-                draggable={false}
-                src={pc}
-                alt="main page banner"
-              />
-            </Show>
-            <Hide above="tablet">
-              <Image
-                hideBelow={'tablet'}
-                draggable={false}
-                src={BANNER_INFO[idx].mobile}
-                alt="main page banner"
-              />
-            </Hide>
-          </Flex>
-        </Fragment>
-      ))}
-    </ImageSlider>
+    <Flex bg={BANNER_INFO[0].bgColor} justifyContent="center">
+      <Image
+        maxW={{ mobile: '500px', tablet: '1440px' }}
+        w="100%"
+        src={isLargerThanTablet ? mainBanner : mainBannerMobile}
+      />
+    </Flex>
   );
 };
 
