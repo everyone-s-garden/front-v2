b2558dde5ed194a580b1661aca44eef994a9613b
HongGunWoo <foxrain.gg@gmail.com>
2024-10-15 19:36:51 +0900

    feat: 날씨 아이콘 조건에 따라 분기 로직 추가

--- src/pages/Main/utils/getWeatherIcon.ts
+++ src/pages/Main/utils/getWeatherIcon.ts
@@ -8,13 +8,37 @@ import {
   sunnyIcon,
   mainMostlyCloudyIcon,
   mainSunnyIcon,
+  mainCloudyIcon,
+  mainRainIcon,
+  mainRainSnowIcon,
+  mainShowerIcon,
+  mainSnowIcon,
 } from '@/assets/icons/weather';
 
 const getWeatherIcon = (weather: string, main?: boolean): string => {
+  if (main) {
+    switch (weather) {
+      case '맑음':
+        return mainSunnyIcon;
+      case '비':
+        return mainRainIcon;
+      case '비/눈':
+        return mainRainSnowIcon;
+      case '눈':
+        return mainSnowIcon;
+      case '구름많음':
+        return mainMostlyCloudyIcon;
+      case '소나기':
+        return mainShowerIcon;
+      case '흐림':
+        return mainCloudyIcon;
+      default:
+        return mainSunnyIcon;
+    }
+  }
+
   switch (weather) {
     case '맑음':
-      if (main) return mainSunnyIcon;
-
       return sunnyIcon;
     case '비':
       return rainIcon;
@@ -23,8 +47,6 @@ const getWeatherIcon = (weather: string, main?: boolean): string => {
     case '눈':
       return snowIcon;
     case '구름많음':
-      if (main) return mainMostlyCloudyIcon;
-
       return mostlyCloudyIcon;
     case '소나기':
       return showerIcon;
