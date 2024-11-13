98f358482474b36052b0e6e295024286a4c396e7
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-11-13 20:44:11 +0900

    style: 등급 이미지 변경

--- src/components/Grade/Grade.tsx
+++ src/components/Grade/Grade.tsx
@@ -5,37 +5,31 @@ const gradeData = [
   {
     image: Seed,
     grade: '씨앗 등급',
-    color: '#FF9029',
     description: '회원 가입 시 씨앗 등급',
   },
   {
     image: Sprout,
     grade: '새싹 등급',
-    color: '#296512',
     description: '댓글 5회, 글 1회 이상 시 새싹등급',
   },
   {
     image: Stem,
     grade: '가지 등급',
-    color: '#83A834',
     description: '댓글 10회, 글 5회 이상 시 가지등급',
   },
   {
     image: Fruit,
     grade: '열매 등급',
-    color: '#F6C857',
     description: '글 15회 이상, 거래 1회 이상 시 열매 등급',
   },
   {
     image: Harvest,
     grade: '수확 등급',
-    color: '#C15201',
     description: '거래 5회 이상 시 수확 등급',
   },
   {
     image: Farmer,
     grade: '농사꾼 등급',
-    color: 'black',
     description: '거래 10회 이상 시 농사꾼 등급',
   },
 ];
@@ -46,8 +40,8 @@ const Grade = () => {
       <Text fontSize={{ mobile: '16px', tablet: '20px' }} fontWeight={'bold'}>
         모두의 텃밭 등급제도
       </Text>
-      <List mt={'48px'} maxW={'300px'} w={'100%'}>
-        {gradeData.map(({ image, grade, description, color }) => (
+      <List mt={{ mobile: '35px', tablet: '48px' }} maxW={'300px'} w={'100%'}>
+        {gradeData.map(({ image, grade, description }) => (
           <ListItem
             key={grade}
             display={'flex'}
@@ -61,11 +55,10 @@ const Grade = () => {
             <Image
               src={image}
               alt={grade}
-              h={{ mobile: '44px', tablet: '56px' }}
+              w={{ mobile: '45.72px', tablet: '54.28px' }}
             />
             <Flex flexDir={'column'} gap={'4px'}>
               <Text
-                color={color}
                 fontWeight={'semiBold'}
                 fontSize={{ mobile: '14px', tablet: '16px' }}
               >
