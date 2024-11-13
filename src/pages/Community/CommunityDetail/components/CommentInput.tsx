403962fb859db05fa74fadfd85b923c025d53144
hyoribogo <97094709+hyoribogo@users.noreply.github.com>
2024-11-13 20:35:01 +0900

    style: 댓글 버튼 분리

--- src/pages/Community/CommunityDetail/components/CommentInput.tsx
+++ src/pages/Community/CommunityDetail/components/CommentInput.tsx
@@ -1,23 +1,25 @@
 import { Flex, FlexProps, Input, Text } from '@chakra-ui/react';
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { AvatarComponent } from '@/components';
-import { DUMMY_MY_INFO } from '@/data/dummyData';
+import { User } from '../../types';
 import useLoginStore from '@/stores/useLoginStore';
 
 interface CommentInputProps extends FlexProps {
   commentId?: number;
   autoFocus?: boolean;
   handleSubmitComment: (content: string, parentCommentId?: number) => void;
+  userInfo: User;
 }
 
 const CommentInput = ({
   commentId,
   autoFocus,
   handleSubmitComment,
+  userInfo,
   ...rest
 }: CommentInputProps) => {
   const [focus, setFocus] = useState(false);
-  const commentRef = useRef<HTMLInputElement>(null);
+  const [value, setValue] = useState('');
 
   const isLoggedIn = useLoginStore((state) => state.isLoggedIn);
 
@@ -25,11 +27,26 @@ const CommentInput = ({
   return (
     <Flex gap={{ mobile: '12px', tablet: '16px' }} align={'center'} {...rest}>
       <AvatarComponent
-        src={DUMMY_MY_INFO.profile ?? ''}
+        src={userInfo?.profile ?? ''}
         w={{ mobile: '36px', tablet: '44px' }}
         h={{ mobile: '36px', tablet: '44px' }}
       />
-      <Flex
+      <Input
+        variant={'unstyled'}
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        fontSize={{ mobile: '14px', tablet: '16px' }}
+        fontWeight={'medium'}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+          }
+        }}
+        autoFocus={autoFocus}
+        disabled={!isLoggedIn}
+        placeholder={
+          isLoggedIn ? undefined : '로그인 후 댓글을 작성할 수 있습니다.'
+        }
         h={{ mobile: '36px', tablet: '42px' }}
         px={{ mobile: '14px', tablet: '16px' }}
         py={{ mobile: '10px', tablet: '12px' }}
@@ -37,44 +54,31 @@ const CommentInput = ({
         border={'1px solid'}
         borderColor={focus ? 'green.500' : 'gray.100'}
         flexGrow={1}
-        align={'center'}
-        gap={2}
-      >
-        <Input
-          variant={'unstyled'}
-          borderRadius={0}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-          fontSize={{ mobile: '14px', tablet: '16px' }}
-          fontWeight={'medium'}
-          ref={commentRef}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              e.preventDefault();
-            }
-          }}
-          autoFocus={autoFocus}
-          disabled={!isLoggedIn}
-          placeholder={
-            isLoggedIn ? undefined : '로그인 후 댓글을 작성할 수 있습니다.'
-          }
-        />
-        <Text
-          fontSize={{ mobile: '14px', tablet: '16px' }}
-          fontWeight={'medium'}
-          color={'gray.400'}
-          flexShrink={0}
-          cursor={isLoggedIn ? 'pointer' : 'not-allowed'}
-          onClick={() => {
-            if (!isLoggedIn) return;
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <Text
+        h={{ mobile: '36px', tablet: '42px' }}
+        w={{ mobile: '60px', tablet: '72px' }}
+        borderRadius={'8px'}
+        bgColor={value ? 'green.500' : 'gray.100'}
+        display={'flex'}
+        alignItems={'center'}
+        justifyContent={'center'}
+        fontSize={{ mobile: '14px', tablet: '16px' }}
+        fontWeight={'medium'}
+        color={value ? 'white' : 'gray.400'}
+        flexShrink={0}
+        cursor={isLoggedIn ? 'pointer' : 'not-allowed'}
+        onClick={() => {
+          if (!isLoggedIn) return;
 
-            handleSubmitComment(commentRef.current?.value ?? '', commentId);
-            commentRef.current!.value = '';
-          }}
-        >
-          입력
-        </Text>
-      </Flex>
+          handleSubmitComment(value ?? '', commentId);
+          setValue('');
+        }}
+      >
+        입력
+      </Text>
     </Flex>
   );
 };
