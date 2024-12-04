bb74df2f13a9dc31c117b1aa9eede6da0021793f
mac <mac@macui-MacBookAir.local>
2024-12-04 20:02:23 +0900

    feat: ga tag 심기완료

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,29 @@
 import { useMediaQuery } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import ReactGA from 'react-ga4';
+import { useLocation } from 'react-router-dom';
 import MobileHeader from './MobileHeader';
 import PcHeader from './PcHeader';
 import { devices } from '@/styles/theme';
 
 const Header = () => {
   const [isLargerThanTablet] = useMediaQuery(devices.tablet);
+  const [initialized, setInitialized] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!window.location.href.includes('localhost')) {
+      ReactGA.initialize(import.meta.env.VITE_GOOGLE_ANALYTICS_TRAKING_ID);
+      setInitialized(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (initialized) {
+      ReactGA.set({ page: location.pathname });
+      ReactGA.send('pageview');
+    }
+  }, [initialized, location]);
 
   return (
     <header>{isLargerThanTablet ? <PcHeader /> : <MobileHeader />}</header>
